395122bcdad0c36a9af5550e128194c4d694da10
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-22 03:35:38 +0530

    Added modules and setup.py

---
+++ hello_world/hello.py
@@ -0,0 +1,8 @@
+#! /usr/bin python
+
+def foo():
+    print("Foo was called!")
+
+def bar():
+    print("Bar was called!")
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, Extension
+
+if sys.argv[-1] == 'setup.py':
+    print('To install, run \'python setup.py install\'')
+    print()
+
+entry_points = {
+    'console_scripts': [
+        'foo = hello_world.hello:foo',
+        'bar = hello_world.hello:bar',
+    ]
+}
+
+if __name__ == "__main__":
+
+    setup(
+        name               = 'hello-world',
+        version            = '1.0',
+        maintainer         = 'Himanshu Mishra',
+        maintainer_email   = 'himanshumishra@iitkgp.ac.in',
+        description        = 'Testing python programs',
+        packages           = ['hello_world'],
+        entry_points       = entry_points,
+        test_suite         = 'nose.collector',
+        tests_require      = ['nose>=0.10.1']
+    )
